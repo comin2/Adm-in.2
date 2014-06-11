c859bb72d824acca8ca350795fb0de5c557ae11b
viki53 <viki53000@gmail.com>
2014-06-11 17:53:57 +0200

    Adm'in.2
    First commit

---
+++ index.php
@@ -0,0 +1,235 @@
+<?php
+require_once './css-crush/CssCrush.php';
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Adm'in.2</title>
+	
+	<meta name="viewport" content="initial-scale=1, user-scalable=no">
+	<?php echo csscrush_tag('css/adm-in-2.css'); ?>
+</head>
+<body>
+	<div id="topbar">
+		<a href="./" id="logo"><span>Adm'in.2</span></a>
+
+		<nav id="quick-links">
+			<a href="#0"><span>Mon profil</span></a>
+			<a href="#0"><span>Déconnexion</span></a>
+		</nav>
+	</div>
+	<nav id="sidebar" hidden="true" aria-hidden="true">
+		<header id="sidebar-header" role="button" tabindex="0"><h1>Menu</h1></header>
+
+		<ul id="sidebar-content">
+			<li class="widget widget-search">
+				<h2 class="widget-title">Recherche</h2>
+				<form action="#0" method="get" class="widget-content show" id="sidebar-search-form">
+					<input type="search" name="q" value="post(author:viki53)" placeholder="Rechercher">
+				</form>
+			</li>
+			<li class="menu">
+				<h2 class="menu-title"><a href="#0">Articles</a></h2>
+				<ul class="menu-content">
+					<li class="submenu">
+						<a href="#0">Liste <span class="badge badge-warning">1</span></a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Ajouter</a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Catégories <small class="submenu-description">Une description par ici</small></a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu">
+				<h2 class="menu-title"><a href="#0">Médias <span class="badge">2</span></a></h2>
+				<ul class="menu-content">
+					<li class="submenu">
+						<a href="#0">Liste <span class="badge badge-info">2</span></a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Ajouter</a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Catégories</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu">
+				<h2 class="menu-title"><a href="#0">Utilisateurs</a></h2>
+				<ul class="menu-content">
+					<li class="submenu">
+						<a href="#0">Liste <span class="badge badge-success">5</span></a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Membres premium</a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Ajouter</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu">
+				<h2 class="menu-title"><a href="#0">Défis</a></h2>
+				<ul class="menu-content">
+					<li class="submenu">
+						<a href="#0">Liste</a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Ajouter</a>
+					</li>
+				</ul>
+			</li>
+			<li class="menu">
+				<h2 class="menu-title"><a href="#0">Paramètres</a></h2>
+				<ul class="menu-content">
+					<li class="submenu">
+						<a href="#0">API <span class="badge badge-error">2</span></a>
+					</li>
+					<li class="submenu">
+						<a href="#0">Administrateurs</a>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+	<div id="main">
+		<h1>Tableau de bord</h1>
+		<div class="row">
+			<div class="column-large widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Statistiques</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-small widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>En ligne</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Personne</p>
+					</div>
+				</section>
+			</div>
+		</div>
+		<div class="row">
+			<div class="column-tiny widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Accès rapides</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-big widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Brouillon rapide</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+		</div>
+		<div class="row">
+			<div class="column-medium widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Derniers inscrit</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-medium widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Derniers articles</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+		</div>
+		<div class="row">
+			<div class="column-tiny widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Titre</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-medium widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Titre</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-tiny widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Titre</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+		</div>
+		<div class="row">
+			<div class="column-small widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Titre</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-small widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Titre</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+			<div class="column-small widgets-column">
+				<section class="widget">
+					<header class="widget-header">
+						<h2>Titre</h2>
+					</header>
+					<div class="widget-content">
+						<p class="italic">Pas de contenu</p>
+					</div>
+				</section>
+			</div>
+		</div>
+		<div id="footer">
+			<p>Nom du site — <?php echo date('Y'); ?></p>
+		</div>
+	</div>
+	<script src="js/adm-in-2.js"></script>
+</body>
+</html>
---
+++ js/adm-in-2.js
@@ -0,0 +1,60 @@
+var sidebar = document.querySelector('#sidebar');
+var sidebar_menu_titles = document.querySelectorAll('#sidebar .menu>.menu-title');
+
+document.body.classList.add('js-enabled');
+
+for (var i=0, nb=sidebar_menu_titles.length; i<nb; i++) {
+	var toggle_btn = document.createElement('button');
+	toggle_btn.type = 'button';
+	toggle_btn.textContent = '-';
+	toggle_btn.title = 'Cacher le contenu de ce menu';
+	toggle_btn.className = 'menu-toggle';
+
+	sidebar_menu_titles[i].insertBefore(toggle_btn, sidebar_menu_titles[i].firstElementChild);
+	sidebar_menu_titles[i].appendChild(toggle_btn);
+}
+
+function admin2DocumentClick(e){
+	if (e.target.id === 'sidebar-header') {
+		if (sidebar.getAttribute('hidden')) {
+			sidebar.removeAttribute('hidden');
+			sidebar.removeAttribute('aria-hidden');
+		}
+		else {
+			sidebar.setAttribute('hidden', true);
+			sidebar.setAttribute('aria-hidden', true);
+		}
+		e.target.blur();
+		return true;
+	}
+	if (e.target.classList.contains('menu-toggle')) {
+		e.stopPropagation();
+		var elem = e.target.parentNode;
+
+		// console.dir(e);
+
+		while ((!elem.classList || !elem.classList.contains('menu-content')) && elem.nextSibling) {
+			elem = elem.nextSibling;
+		}
+
+		if (!elem.classList.contains('menu-content')) {
+			return false;
+		}
+
+		if (elem.getAttribute('hidden')) {
+			elem.removeAttribute('hidden');
+			elem.removeAttribute('aria-hidden');
+			e.target.textContent = '-';
+			e.target.title = 'Cacher le contenu de ce menu';
+		}
+		else {
+			elem.setAttribute('hidden', true);
+			elem.setAttribute('aria-hidden', true);
+			e.target.textContent = '+';
+			e.target.title = 'Afficher le contenu de ce menu';
+		}
+		return true;
+	}
+}
+
+document.addEventListener('click', admin2DocumentClick, true);
