1507a6facf2e430b4fecc8d46a696b0c9ecec065
viki53 <viki53000@gmail.com>
2014-06-28 13:58:18 +0200

    Disabled range input

--- index.php
+++ index.php
@@ -212,6 +212,7 @@ if(!empty($_GET['q'])) {
 						<div>
 							<label for="demo-input-range" class="full">Range input</label>
 							<input type="range" name="custom-range" value="5" min="0" max="10" class="input-full" id="demo-input-range">
+							<input type="range" name="custom-range" value="5" min="0" max="10" class="input-full" id="demo-input-range" disabled>
 						</div>
 					</div>
 				</section>
