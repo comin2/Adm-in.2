eebae21fb261e051b1ffd78f4ee8b88a8ce6464b
viki53 <viki53000@gmail.com>
2014-07-05 13:28:23 +0200

    JS: draggables event delegation

--- js/adm-in-2_plugin_draggables.js
+++ js/adm-in-2_plugin_draggables.js
@@ -1,16 +1,17 @@
 Admin2.Draggables = {
-	handleDragStart: function (e) {
-		this.classList.add('dragged');
+	handleDragStart: function (event) {
+		if(!event.target.draggable) {
+			return false;
+		}
+		event.target.classList.add('dragged');
 	},
-	handleDragEnd: function (e) {
-		this.classList.remove('dragged');
+	handleDragEnd: function (event) {
+		if(!event.target.draggable) {
+			return false;
+		}
+		event.target.classList.remove('dragged');
 	}
 }
 
-Admin2.draggable_elements = document.querySelectorAll('[draggable="true"]');
-console.dir(Admin2.draggable_elements);
-
-[].forEach.call(Admin2.draggable_elements, function(draggable_elem) {
-	draggable_elem.addEventListener('dragstart', Admin2.Draggables.handleDragStart, false);
-	draggable_elem.addEventListener('dragend', Admin2.Draggables.handleDragEnd, false);
-});
+document.addEventListener('dragstart', Admin2.Draggables.handleDragStart, false);
+document.addEventListener('dragend', Admin2.Draggables.handleDragEnd, false);
