27b444e2326cae5f917cb5813471775f7ba927aa
viki53 <viki53000@gmail.com>
2015-04-03 12:07:28 +0200

    Remove voice plugin auto start

--- js/adm-in-2_plugin_voice.js
+++ js/adm-in-2_plugin_voice.js
@@ -1,18 +1,18 @@
 
 if (!('webkitSpeechRecognition' in window)) {
-	console.log('Your browser doesn\'t support speech recognition. That\‘s too bad…');
+	console.error('Your browser doesn\'t support speech recognition. That\‘s too bad…');
 }
 else {
-	var recognition = new webkitSpeechRecognition();
-	recognition.continuous = true;
-	recognition.interimResults = true;
-	recognition.lang = Admin2.html.lang || navigator.language;
+	Admin2.voiceRecognition = new webkitSpeechRecognition();
+	Admin2.voiceRecognition.continuous = true;
+	Admin2.voiceRecognition.interimResults = true;
+	Admin2.voiceRecognition.lang = Admin2.html.lang || navigator.language;
 
-	recognition.onstart = function(event) {
+	Admin2.voiceRecognition.onstart = function(event) {
 		// console.log(event);
 		console.log('I am listening…');
 
-		recognition.onresult = function(event) {
+		Admin2.voiceRecognition.onresult = function(event) {
 			// console.log(event);
 
 			interim_transcript = '';
@@ -25,50 +25,51 @@ else {
 				}
 			}
 		}
-		recognition.onerror = function(event) {
+		Admin2.voiceRecognition.onerror = function(event) {
 			// console.log(event);
 		}
 	}
-	recognition.onend = function(event) {
+	Admin2.voiceRecognition.onend = function(event) {
 		console.log(event);
 		console.log('Goodbye');
-		// recognition.start();
 	}
 
-	recognition.start();
-}
-
-if(typeof Admin2.SPEECH_KEYWORDS !== "object") {
-	Admin2.SPEECH_KEYWORDS = {};
-}
-if(typeof SPEECH_PROCESSES !== "object") {
-	Admin2.SPEECH_PROCESSES = {};
-}
-
-Admin2.SPEECH_KEYWORDS.search = new RegExp(/^(search(ing)?|looking for) /gi);
-
-Admin2.SPEECH_PROCESSES.search = function(searchStr, keyword, regexp) {
-	console.log('Search: "'+searchStr+'"');
-
-	if (!searchStr) {
-		return false;
+	if (Admin2.activateVoicePlugin) {
+		Admin2.voiceRecognition.start();
 	}
-	sidebar_search_input.value = '';
-	sidebar_search_input.value = searchStr;
 
-	sidebar_search_form.submit();
-};
+	if (typeof Admin2.SPEECH_KEYWORDS !== "object") {
+		Admin2.SPEECH_KEYWORDS = {};
+	}
+	if (typeof SPEECH_PROCESSES !== "object") {
+		Admin2.SPEECH_PROCESSES = {};
+	}
 
-Admin2.processSpeech = function (speechStr) {
-	var results;
-	speechStr = speechStr.trim();
-	console.log('Process speech : '+speechStr);
+	Admin2.SPEECH_KEYWORDS.search = new RegExp(/^(search(ing)?|looking for) /gi);
 
-	for (var i in Admin2.SPEECH_KEYWORDS) {
-		results = speechStr.match(Admin2.SPEECH_KEYWORDS[i]);
-		console.dir(results);
-		if(results) {
-			Admin2.SPEECH_PROCESSES[i](speechStr.replace(results[0], '').trim(), results[0], Admin2.SPEECH_KEYWORDS[i]);
+	Admin2.SPEECH_PROCESSES.search = function(searchStr, keyword, regexp) {
+		console.log('Search: "'+searchStr+'"');
+
+		if (!searchStr) {
+			return false;
+		}
+		sidebar_search_input.value = '';
+		sidebar_search_input.value = searchStr;
+
+		sidebar_search_form.submit();
+	};
+
+	Admin2.processSpeech = function (speechStr) {
+		var results;
+		speechStr = speechStr.trim();
+		console.log('Process speech : '+speechStr);
+
+		for (var i in Admin2.SPEECH_KEYWORDS) {
+			results = speechStr.match(Admin2.SPEECH_KEYWORDS[i]);
+			console.dir(results);
+			if(results) {
+				Admin2.SPEECH_PROCESSES[i](speechStr.replace(results[0], '').trim(), results[0], Admin2.SPEECH_KEYWORDS[i]);
+			}
 		}
 	}
 }
